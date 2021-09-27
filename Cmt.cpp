031b4020c38888e25123be6e8f2191e871bc4048
Ritu Raj Jha <85738906+Ritxam@users.noreply.github.com>
2021-09-27 20:14:41 +0530

    Add files via upload

---
+++ Cmt.cpp
@@ -0,0 +1,17 @@
+//preprocessor directive
+#include<iostream>
+
+//telling the compiler what namespace to search in
+using namespace std;
+
+/*now we will write 
+  the main function*/
+int main()
+{
+  //a simple command to print output
+  cout<<"Program to check how comments and namespace works"<<endl;
+  
+  /*returning a 
+    value to OS*/
+  return 0;
+}
